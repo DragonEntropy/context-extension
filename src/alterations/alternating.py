29b86822d1f9ef706225a7fd9fb24d3fd08dcde1
root <alex.jephtha@gmail.com>
2025-10-29 06:17:43 +0000

    fixing alibi + RoPE hybrid integration

--- src/alterations/alternating.py
+++ src/alterations/alternating.py
@@ -1,11 +1,11 @@
 from transformers.models.llama.modeling_llama import LlamaForCausalLM, LlamaAttention, LlamaConfig
 
-from alterations.ALiBi import LlamaAttentionALiBi
+from alterations.ALiBi import LlamaAttentionALiBi, LlamaALiBiConfig
 from alterations.NoPE import LlamaAttentionNoPE
 
 
-class LlamaAlternatingConfig(LlamaConfig):
-    def __init__(self, alt_pattern=["nope", "rope"], **kwargs):
+class LlamaAlternatingConfig(LlamaALiBiConfig):
+    def __init__(self, alt_pattern=["alibi", "rope"], **kwargs):
         super().__init__(**kwargs)
         self.alt_pattern = alt_pattern
 
@@ -26,7 +26,8 @@ class LlamaAlternatingForCausalLM(LlamaForCausalLM):
                     print(f"Configuring layer {i} to NoPE")
                     layer.self_attn = LlamaAttentionNoPE(config, i)
                 elif attention_type == "rope":
+                    # No need to alter
                     print(f"Configuring layer {i} to RoPE")
-                    layer.self_attn = LlamaAttention(config, i)
+                    # layer.self_attn = LlamaAttention(config, i)
                 else:
                     assert False, f"Unimplemented attention type {attention_type}."
